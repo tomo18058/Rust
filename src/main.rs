1e39d52c162733637cce0ccf3e701026a7de3fe8
原知輝 <kuri12monbran03@au.com>
2025-05-13 15:38:55 +0900

    Initial commit: Rust learning - variables

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+fn main() {
+    // addition
+    // 足し算
+    let sum = 5 + 10;
+    println!("{}",sum);
+
+    // subtraction
+    // 引き算
+    let difference = 95.5 - 4.3;
+    println!("{}",difference);
+
+    // multiplication
+    // 掛け算
+    let product = 4 * 30;
+    println!("{}",product);
+
+    // division
+    // 割り算
+    let quotient = 56.7 / 32.2;
+    let floored = 2 / 3; // Results in 0
+                              // 結果は0
+    println!("{}",quotient);
+    println!("{}",floored);
+
+    // remainder
+    // 余り
+    let remainder = 43 % 5;
+    println!("{}",remainder);
+
+    
+    // 文字型
+    let a = 'z';
+    let b = 'Z';
+    let heart_eyed_cat = '😻';
+    println!("{}",a);
+    println!("{}",b);
+    println!("{}",heart_eyed_cat);
+
+    
+}
+
